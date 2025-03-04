3ff1f0d395304d719092b4d9e5db5fb968aa7937
Muhammad Saad <165696604+saad-waseem7@users.noreply.github.com>
2025-03-04 21:07:22 +0500

    Add files via upload

---
+++ Files/Form1.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace App_1
+{
+
+    public partial class Form1 : Form
+    {
+        SqlConnection connect = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\hp\OneDrive\Documents\logindata.mdf;Integrated Security=True;Connect Timeout=30");
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label7_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void signup_LOGIN_Click(object sender, EventArgs e)
+        {
+            Frm_Login login_register_here = new Frm_Login();
+            login_register_here.Show();
+            this.Hide();
+            
+        }
+
+        private void signup_close_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void signup_btn_Click(object sender, EventArgs e)
+        {
+            if (signup_email.Text == "" || signup_username.Text == ""
+                || signup_password.Text == "")
+            {
+                MessageBox.Show("Please fill all blank fields", "Error Message", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            else
+            {
+                if (connect.State != ConnectionState.Open)
+                {
+                    try
+                    {
+                        connect.Open();
+                        String checkUsername = "SELECT * FROM admin WHERE username = '"
+                            + signup_username.Text.Trim() + "'"; // admin is our table name
+
+                        using (SqlCommand checkUser = new SqlCommand(checkUsername, connect))
+                        {
+                            SqlDataAdapter adapter = new SqlDataAdapter(checkUser);
+                            DataTable table = new DataTable();
+                            adapter.Fill(table);
+
+                            if (table.Rows.Count >= 1)
+                            {
+                                MessageBox.Show(signup_username.Text + " is already exist", "Error Message", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                            }
+                            else
+                            {
+                                string insertData = "INSERT INTO admin (email, username, _password, date_created) " +
+                                    "VALUES(@email, @username, @pass, @date)";
+
+                                DateTime date = DateTime.Today;
+
+                                using (SqlCommand cmd = new SqlCommand(insertData, connect))
+                                {
+                                    cmd.Parameters.AddWithValue("@email", signup_email.Text.Trim());
+                                    cmd.Parameters.AddWithValue("@username", signup_username.Text.Trim());
+                                    cmd.Parameters.AddWithValue("@pass", signup_password.Text.Trim());
+                                    cmd.Parameters.AddWithValue("@date", date);
+
+                                    cmd.ExecuteNonQuery();
+
+                                    MessageBox.Show("Registered successfully", "Information Message", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+                                    // TO SWITCH THE FORM 
+                                    Form1 signup_LOGIN = new Form1();
+                                    signup_LOGIN.Show();
+                                    this.Hide();
+                                }
+                            }
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show("Error connecting Database: " + ex, "Error Message", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                    finally
+                    {
+                        connect.Close();
+                    }
+                }
+
+            }
+        }
+
+        private void signup_show_password_CheckedChanged(object sender, EventArgs e)
+        {
+            if (signup_show_password.Checked)
+            {
+                signup_password.PasswordChar = '\0';
+            }
+            else
+            {
+                signup_password.PasswordChar = '*';
+            }
+        }
+
+        private void signup_email_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ Files/Frm_Login.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace App_1
+{
+    public partial class Frm_Login : Form
+    {
+        SqlConnection connect = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\hp\OneDrive\Documents\logindata.mdf;Integrated Security=True;Connect Timeout=30");
+        public Frm_Login()
+        {
+            InitializeComponent();
+        }
+
+        private void Frm_Login_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void login_register_here_Click(object sender, EventArgs e)
+        {
+            Form1 signup_LOGIN = new Form1();
+            signup_LOGIN.Show();
+            this.Hide();
+        }
+
+        private void login_close_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void login_btn_Click(object sender, EventArgs e)
+        {
+            if (login_username.Text == "" || login_password.Text == "")
+            {
+                MessageBox.Show("Please fil all blank fields", "Error Message", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            else
+            {
+                if (connect.State != ConnectionState.Open)
+                {
+                    try
+                    {
+                        connect.Open();
+
+                        String selectData = "SELECT * FROM admin WHERE username = @username AND _password = @pass";
+                        using (SqlCommand cmd = new SqlCommand(selectData, connect))
+                        {
+                            cmd.Parameters.AddWithValue("@username", login_username.Text);
+                            cmd.Parameters.AddWithValue("@pass", login_password.Text);
+                            SqlDataAdapter adapter = new SqlDataAdapter(cmd);
+                            DataTable table = new DataTable();
+                            adapter.Fill(table);
+
+                            if (table.Rows.Count >= 1)
+                            {
+                                MessageBox.Show("Logged In successfully", "Information Message", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+                                MainForm mForm = new MainForm();
+                                mForm.Show();
+                                this.Hide();
+                            }
+                            else
+                            {
+                                MessageBox.Show("Incorrect Username/Password", "Error Message", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                            }
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show("Error Connecting: " + ex, "Error Message", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                    finally
+                    {
+                        connect.Close();
+                    }
+                }
+            }
+        }
+
+        private void log_show_password_CheckedChanged(object sender, EventArgs e)
+        {
+            if (log_show_password.Checked)
+            {
+                login_password.PasswordChar = '\0';
+            }
+            else
+            {
+                login_password.PasswordChar = '*';
+            }
+        }
+
+    }
+}
